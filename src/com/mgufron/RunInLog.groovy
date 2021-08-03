9871b17af244dac85ce95450879751c42568a17a
Mochamad Gufron <mgufronefendi@gmail.com>
2021-08-03 19:30:23 +0700

    update messages

--- src/com/mgufron/RunInLog.groovy
+++ src/com/mgufron/RunInLog.groovy
@@ -5,10 +5,10 @@ class RunInLog {
   RunInLog(slack) {
     this.slack = slack
   }
-  void send(int id, String msg) {
-    slack.update(id, slack.message(msg))
+  void send(int id, String msg, Map accessory = null) {
+    slack.update(id, slack.message(msg), accessory)
   }
-  void run(Map msg, Closure body) {
+  void run(Map msg, Closure body, Map failAccessory = null) {
     try {
       if(!msg["id"] || msg["id"] == null) {
         msg["id"] = slack.preserveBlock()
@@ -17,7 +17,7 @@ class RunInLog {
       body.call()
       send(msg["id"], ":white_check_mark: ${msg["message"]}")
     } catch(e) {
-      send(msg["id"], ":no_entry: ${msg["message"]}")
+      send(msg["id"], ":no_entry: ${msg["message"]}", failAccessory)
       throw e
     }
   }
