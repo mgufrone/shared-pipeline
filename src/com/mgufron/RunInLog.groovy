2eb2c3e7fb14c9872812c175bc6e984a5696413a
Mochamad Gufron <mgufronefendi@gmail.com>
2021-08-03 21:11:44 +0700

    update pipeline

--- src/com/mgufron/RunInLog.groovy
+++ src/com/mgufron/RunInLog.groovy
@@ -1,23 +1,35 @@
 package com.mgufron
 import com.mgufron.SlackSectionedMessage
+import groovy.time.TimeCategory
+
 class RunInLog {
   final SlackSectionedMessage slack
-  RunInLog(slack) {
+  final boolean withBenchmark
+  RunInLog(slack, boolean withBenchmark = false) {
     this.slack = slack
+    this.withBenchmark = withBenchmark
   }
   void send(int id, String msg, Map accessory = null) {
     slack.update(id, slack.message(msg, accessory))
   }
+  private String runDuration(Date start) {
+    def dur = groovy.time.TimeCategory.minus(
+      new Date(),
+      start
+    )
+    return "${dur.seconds}s"
+  }
   void run(Map msg, Closure body) {
+    def start = new Date()
     try {
       if(!msg["id"] || msg["id"] == null) {
         msg["id"] = slack.preserveBlock()
       }
       send(msg["id"], ":gh-loading: ${msg["message"]}")
       body.call()
-      send(msg["id"], ":white_check_mark: ${msg["message"]}")
+      send(msg["id"], ":white_check_mark: ${msg["message"]} (${runDuration(start)})")
     } catch(e) {
-      send(msg["id"], ":no_entry: ${msg["message"]}", msg["failBtn"])
+      send(msg["id"], ":no_entry: ${msg["message"]} (${runDuration(start)})", msg["failBtn"])
       throw e
     }
   }
