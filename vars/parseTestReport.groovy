48af7bb053eeb583b20196733053785c2154f0ce
Mochamad Gufron <mgufronefendi@gmail.com>
2021-08-05 09:29:13 +0700

    use plain string

--- vars/parseTestReport.groovy
+++ vars/parseTestReport.groovy
@@ -2,10 +2,6 @@ import com.mgufron.ReporterProcessor
 
 ReporterProcessor call(List<String> reportPaths) {
   def rp = new ReporterProcessor()
-  def files = reportPaths.each {
-    new File(it).getText()
-  }
-  println "${files}"
-  rp.withTexts(files)
+  rp.withTexts(reportPaths)
   return rp
 }
