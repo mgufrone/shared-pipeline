8a342208d5eb30303fa3bec32c274b9ca787eb72
Mochamad Gufron <mgufronefendi@gmail.com>
2021-10-24 17:21:32 +0700

    add missing type

--- src/com/mgufron/Cache.groovy
+++ src/com/mgufron/Cache.groovy
@@ -10,14 +10,16 @@ class Cache {
   Closure restorer
   public Cache(String repoName, String branch, String fallbackBranch, Closure archiver, Closure restorer) {
     MessageDigest sha1 = MessageDigest.getInstance("SHA1")
-    byte[] digest  = sha1.digest("${repoName}-${branch}".getBytes())
+    String password = "$repoName-$branch".toString()
+    byte[] digest  = sha1.digest(password.getBytes())
     String finalSum = new BigInteger(1, digest).toString(16)
     this.archiver = archiver
     this.restorer = restorer
     this.setMainChecksum(finalSum)
     this.setFallbackChecksum(finalSum)
     if (branch != fallbackBranch) {
-      byte[] fallback = sha1.digest("${repoName}-${fallbackBranch}")
+      String fallString = "${repoName}-${fallbackBranch}".toString()
+      byte[] fallback = sha1.digest(fallString.getBytes())
       String fallbackSum = new BigInteger(1, fallback).toString(16)
       this.setFallbackChecksum(fallbackSum)
     }
